3bf3461263217f473b84dbed9c31816bafd3eff3
kstekels <kstekels+github@gmail.com>
2023-08-17 22:56:33 +0300

    Refactoring Atuh

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from 'react-router-dom';
 // import Navigation from "../routes/navigation/navigation.component";
 // import Home from "../routes/home/home.component";
 // import SignIn from "../routes/sign-in/sign-in.component";
-import Navigation from "./routes/navigation/navigation.component";
-import Home from "./routes/home/home.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Navigation from './routes/navigation/navigation.component';
+import Home from './routes/home/home.component';
+import Authentication from './routes/sign-in/authentication.component';
 
 const Shop = () => {
   return <h1>I am the Shop page</h1>;
@@ -13,10 +13,10 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigation />}>
+      <Route path='/' element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="/sign-in" element={<SignIn />} />
+        <Route path='/shop' element={<Shop />} />
+        <Route path='/auth' element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,20 +1,20 @@
-import { Fragment } from "react";
-import { Outlet, Link } from "react-router-dom";
-import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
-import "./navigation.styles.scss";
+import { Fragment } from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
+import './navigation.styles.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
-      <div className="navigation">
-        <Link className="logo-container" to="/">
+      <div className='navigation'>
+        <Link className='logo-container' to='/'>
           <CrwnLogo />
         </Link>
-        <div className="nav-links-container">
-          <Link className="nav-link" to="/shop">
+        <div className='nav-links-container'>
+          <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className="nav-link" to="/sign-in">
+          <Link className='nav-link' to='/auth'>
             Sign In
           </Link>
         </div>
